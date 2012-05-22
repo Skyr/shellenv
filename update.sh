030baf9244abff53ed66ff94818bee0d7580c5fd
Stefan Schlott <stefan@ploing.de>
2012-05-22 08:40:53 +0200

    Use https clone url for oh my zsh

--- update.sh
+++ update.sh
@@ -88,7 +88,7 @@ if binaryInPath zsh git ; then
 	checkLinkToShellenv .zshrc zshrc
 	createFile .zshrc.local
 	if [ ! -d "$HOME/.oh-my-zsh" ] ; then
-		git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+		git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 	fi
   chsh -s `which zsh`
 fi
